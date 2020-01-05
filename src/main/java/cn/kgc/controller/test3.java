b7a8eb89e49843ee9737d1d1d7221ce6922ae89c
raochao2 <1390688176@qq.com>
2020-01-05 15:51:53 +0800

    第四次修改

--- src/main/java/cn/kgc/controller/test3.java
+++ src/main/java/cn/kgc/controller/test3.java
@@ -1,4 +1,13 @@
 package cn.kgc.controller;
 
 public class test3 {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
